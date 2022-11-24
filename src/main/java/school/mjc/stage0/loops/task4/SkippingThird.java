023c1649682e191bb0bac91afdab86364ea2d794
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-24 19:25:01 +0400

    Changes

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -1,7 +1,19 @@
 package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
-    public void printUntilButThird(int lastPrinted) {
 
+    public void printUntilButThird(int lastPrinted) {
+        if(lastPrinted < 0)
+            System.out.print("");
+        if(lastPrinted == 1)
+            System.out.println(1);
+        else {
+            for (int i = 1; i <= lastPrinted; i++) {
+                if (i % 3 == 0)
+                    continue;
+                else
+                    System.out.println(i);
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -1,7 +1,41 @@
 package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
+
+
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+        int firstSum = 0;
+        int secondSum = 0;
+
+        if(lastInRow < numberToSkip)
+            System.out.println("number to skip is bugger then the last");
+        else if (lastInRow < 0)
+            System.out.println("last number in row is negative");
+
+        else if( numberToSkip == lastInRow){
+
+            for (int i = 0; i <= lastInRow ; i++) {
+                secondSum+=i;
+            }
+            System.out.println("skipped sum is " + secondSum);
+            System.out.println("counted sum is " + firstSum);        }
+        else
+        {
+            int j = 0;
+            for (int i = 1; i <= lastInRow; i++) {
+                if(j == numberToSkip - 1) {
+                    secondSum += i;
+                    j = 0;
+                    continue;
+                }
+                else {
+                    firstSum += i;
+                    j++;
+                }
+            }
+            System.out.println("skipped sum is " + secondSum);
+            System.out.println("counted sum is " + firstSum);
+        }
 
     }
 }
